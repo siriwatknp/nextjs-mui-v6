1fc555ebef61458f64ada85a3e1ae99f8847050a
siriwatknp <siriwatkunaporn@gmail.com>
2024-04-10 14:33:44 +0700

    add tailwindcss and pigment-css

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
+import { withPigment } from '@pigment-css/nextjs-plugin';
+import { experimental_extendTheme as extendTheme } from '@mui/material';
+
+const theme = extendTheme();
+
+console.log(theme.colorSchemes.light.palette.action)
+console.log(theme.colorSchemes.dark.palette.action)
+console.log(theme.generateStyleSheets());
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+export default withPigment(nextConfig, {
+  theme,
+  transformLibraries: ['@mui/material'],
+  transformSx: true,
+});
